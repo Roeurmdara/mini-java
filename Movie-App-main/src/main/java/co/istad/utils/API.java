56ad64ef55ca0de1943c9b7cbda87de80e6882ac
Rorurmdara <rouermdara9200@gmail.com>
2026-04-21 13:30:55 +0700

    API

--- Movie-App-main/src/main/java/co/istad/utils/API.java
+++ Movie-App-main/src/main/java/co/istad/utils/API.java
@@ -2,9 +2,9 @@ package co.istad.utils;
 
 public class API {
 
-    public static final String API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTIyZmYzYzNkNDg1Njg0ZDRlZjM1OGE2ZmU5NmYwNCIsIm5iZiI6MTczNjQwODA2MS4wNDE5OTk4LCJzdWIiOiI2NzdmN2JmZGVkNjc5ZTUzZjg3YWY1N2UiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mnLPaeyREXq30Lp426feMUT3L1cVa6xvgy_79rtcUdw";
+    public static final String API_KEY = "4fbc3b6924cd655618ce5d565f0e75f1";
 
     public static final String BASE_URL = "https://api.themoviedb.org/3";
 
-    public static final String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTIyZmYzYzNkNDg1Njg0ZDRlZjM1OGE2ZmU5NmYwNCIsIm5iZiI6MTczNjQwODA2MS4wNDE5OTk4LCJzdWIiOiI2NzdmN2JmZGVkNjc5ZTUzZjg3YWY1N2UiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mnLPaeyREXq30Lp426feMUT3L1cVa6xvgy_79rtcUdw";
+    public static final String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZmJjM2I2OTI0Y2Q2NTU2MThjZTVkNTY1ZjBlNzVmMSIsIm5iZiI6MTc3Njc0NjE0NS4zODYsInN1YiI6IjY5ZTZmZWExYmU4NGRkZDExOWNmNDE2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-WR9PmCYIR7vL6rLkpxEJtK4vHln6hBbHhjerymmOh0";
 }
